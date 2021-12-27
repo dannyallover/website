3ebc44f7cd43aac0943b105f311d77cc533b9c32
Danny Halawi <dannyhalawi@Dannys-MacBook-Pro.local>
2021-12-26 20:12:26 -0800

    reverse conditional

--- js/mobile_check.js
+++ js/mobile_check.js
@@ -1,3 +1,3 @@
-if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   document.location.href = 'temp/mobile-page.html';
 }
